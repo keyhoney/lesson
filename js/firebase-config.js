0f5500724732a88d55446d615c2050432dc98d86
keyhoney <ip9mong@gmail.com>
2025-05-23 08:23:48 +0900

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,7 +5,7 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-d
 const firebaseConfig = {
   apiKey: "AIzaSyC7yqfCKzG9aVho1Y_ATXBfsCtLeiMU_U4",
   authDomain: "lesson-1276d.firebaseapp.com",
-  databaseURL: "https://lesson-1276d-default-rtdb.firebaseio.com/",
+  databaseURL: "https://lesson-1276d.firebaseio.com",
   projectId: "lesson-1276d",
   storageBucket: "lesson-1276d.firebasestorage.app",
   messagingSenderId: "616109953467",
@@ -15,4 +15,10 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
+// document.write() 경고 제거를 위해 WebSocket 전송만 사용하도록 설정
+// (Firebase 모듈 SDK에서는 실험적 옵션으로 폴링 메커니즘 조정)
+if (db.settings) {
+  db.settings({ experimentalForceLongPolling: false, experimentalAutoDetectLongPolling: false });
+}
+export const SCROLL_PATH = '/scrollPosition';
 export const SCROLL_PATH = '/scrollPosition';
